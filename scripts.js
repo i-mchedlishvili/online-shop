0d38302f5f2a8772f45c584789ffe68b467f7a4e
i-mchedlishvili <imchedlishvili32@gmail.com>
2021-05-30 18:06:50 +0400

    sss

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 
-const container = document.querySelector('.search-cont');
+const container = document.querySelector('#search-cont');
 const twoCols = document.querySelector('#twoColumn');
 const threeCols = document.querySelector('#threeColumn');
 const fourColumn = document.querySelector('#fourColumn');
@@ -7,186 +7,7 @@ const fullColsGenerate = document.querySelector('#sortFullCol');
 const sortFourCols = document.querySelector('#sortFourCols');
 
 
-function reloadPage() {
-    const clearPage = document.querySelector('.search-cont');
-    clearPage.innerHTML = '';
-}
-
-function getQuantity() { 
-  reloadPage();
-  let selValue = document.querySelector('#selection');
-  let strUser = selValue.options[selValue.selectedIndex].value;
-  apiURL = 'https://fakestoreapi.com/products?limit=';
-  apiURL += strUser;
-  fetch( apiURL, {method: 'GET'})
-  .then(Response => Response.json())
-  .then(json => { 
-    let template = "";
-    for(let i = 0; i < json.length; i++) {
-        template += `
-<div class="d-flex flex-column col-3 mb-5">
-<div class="special-offer mb-3">
-  <div class="w-75">
-    <img
-      src=${json[i].image}
-      class="w-100"
-    />
-  </div>
-</div>
-<div class="d-flex mb-1">
-  <i class="fas fa-star text-warning"></i>
-  <i class="fas fa-star text-warning"></i>
-  <i class="fas fa-star text-warning"></i>
-  <i class="fas fa-star text-warning"></i>
-  <i class="fas fa-star text-muted"></i>
-</div>
-<p class="">${json[i].title}</p>
-<p class="mb-2 fw-bold">$${json[i].price}</p>
-<div class="d-flex">
-  <i class="fas fa-circle text-info me-2 fs-5"></i>
-  <i class="fas fa-circle text-secondary me-2 fs-5"></i>
-  <i class="fas fa-circle text-dark fs-5"></i>
-</div>
-</div>
-        `;
-    }
-    container.innerHTML += template;
-  })
-  };
-
-  getQuantity();
-
-function generate() {
-  reloadPage();
-  let selValue = document.querySelector('#selection');
-  let strUser = selValue.options[selValue.selectedIndex].value;
-  apiURL = 'https://fakestoreapi.com/products?limit=';
-  apiURL += strUser;
-  fetch( apiURL, {method: 'GET'})
-    .then(res=>res.json())
-    .then(json=> {
-        console.log(json);
-        let template = "";
-        for(let i = 0; i < json.length; i++) {
-          
-            template += `
-    <div class="d-flex flex-column col-3 mb-5">
-    <div class="special-offer mb-3">
-      <div class="w-75">
-        <img
-          src=${json[i].image}
-          class="w-100"
-        />
-      </div>
-    </div>
-    <div class="d-flex mb-1">
-      <i class="fas fa-star text-warning"></i>
-      <i class="fas fa-star text-warning"></i>
-      <i class="fas fa-star text-warning"></i>
-      <i class="fas fa-star text-warning"></i>
-      <i class="fas fa-star text-muted"></i>
-    </div>
-    <p class="">${json[i].title}</p>
-    <p class="mb-2 fw-bold">$${json[i].price}</p>
-    <div class="d-flex">
-      <i class="fas fa-circle text-info me-2 fs-5"></i>
-      <i class="fas fa-circle text-secondary me-2 fs-5"></i>
-      <i class="fas fa-circle text-dark fs-5"></i>
-    </div>
-  </div>
-            `;
-        }
-        container.innerHTML += template;
-    })
-}
-
-
-function generateTwoCols() {
-  reloadPage();
-fetch('https://fakestoreapi.com/products?limit=20')
-.then(res=>res.json())
-.then(json=> {
-    console.log(json);
-    let template = "";
-    for(let i = 0; i < json.length; i++) {
-        console.log(json[i].image)
-        template += `
-<div class="d-flex flex-column col-6 mb-5">
-<div class="special-offer mb-3">
-  <div class="w-75">
-    <img
-      src=${json[i].image}
-      class="w-100"
-    />
-  </div>
-</div>
-<div class="d-flex mb-1">
-  <i class="fas fa-star text-warning"></i>
-  <i class="fas fa-star text-warning"></i>
-  <i class="fas fa-star text-warning"></i>
-  <i class="fas fa-star text-warning"></i>
-  <i class="fas fa-star text-muted"></i>
-</div>
-<p class="">${json[i].title}</p>
-<p class="mb-2 fw-bold">$${json[i].price}</p>
-<div class="d-flex">
-  <i class="fas fa-circle text-info me-2 fs-5"></i>
-  <i class="fas fa-circle text-secondary me-2 fs-5"></i>
-  <i class="fas fa-circle text-dark fs-5"></i>
-</div>
-</div>
-        `;
-    }
-    container.innerHTML += template;
-})
-}
-
-function generateThreeCols() {
-  reloadPage();
-  let selValue = document.querySelector('#selection');
-  let strUser = selValue.options[selValue.selectedIndex].value;
-  apiURL = 'https://fakestoreapi.com/products?limit=';
-  apiURL += strUser;
-  fetch( apiURL, {method: 'GET'})
-.then(res=>res.json())
-.then(json=> {
-    console.log(json);
-    let template = "";
-    for(let i = 0; i < json.length; i++) {
-        console.log(json[i].image)
-        template += `
-<div class="d-flex flex-column col-4 mb-5">
-<div class="special-offer mb-3">
-  <div class="w-75">
-    <img
-      src=${json[i].image}
-      class="w-100"
-    />
-  </div>
-</div>
-<div class="d-flex mb-1">
-  <i class="fas fa-star text-warning"></i>
-  <i class="fas fa-star text-warning"></i>
-  <i class="fas fa-star text-warning"></i>
-  <i class="fas fa-star text-warning"></i>
-  <i class="fas fa-star text-muted"></i>
-</div>
-<p class="">${json[i].title}</p>
-<p class="mb-2 fw-bold">$${json[i].price}</p>
-<div class="d-flex">
-  <i class="fas fa-circle text-info me-2 fs-5"></i>
-  <i class="fas fa-circle text-secondary me-2 fs-5"></i>
-  <i class="fas fa-circle text-dark fs-5"></i>
-</div>
-</div>
-        `;
-    }
-    container.innerHTML += template;
-})
-}
-
-function fullColls() {
-reloadPage();
+function generateColls() {
 let selValue = document.querySelector('#selection');
   let strUser = selValue.options[selValue.selectedIndex].value;
   apiURL = 'https://fakestoreapi.com/products?limit=';
@@ -194,13 +15,11 @@ let selValue = document.querySelector('#selection');
   fetch( apiURL, {method: 'GET'})
 .then(res=>res.json())
 .then(json=> {
-    console.log(json);
     let template = "";
     for(let i = 0; i < json.length; i++) {
-        console.log(json[i].image)
         template += `
-<div class="d-flex col-12 mb-5">
-<div class="special-offer mb-3 w-25 me-3">
+<div class="d-flex flex-column col mb-5">
+<div class="special-offer mb-2">
   <div class="w-100">
     <img
       src=${json[i].image}
@@ -208,16 +27,17 @@ let selValue = document.querySelector('#selection');
     />
   </div>
 </div>
-<div class="d-flex flex-column justify-content-center">
+<div class="d-flex ms-2 flex-column justify-content-center">
 <div class="d-flex mb-1">
   <i class="fas fa-star text-warning"></i>
   <i class="fas fa-star text-warning"></i>
   <i class="fas fa-star text-warning"></i>
   <i class="fas fa-star text-warning"></i>
-  <i class="fas fa-star text-muted"></i>
+  <i class="fas fa-s
+  tar text-muted"></i>
 </div>
 <p class="fw-bold text-muted">${json[i].title}</p>
-<p class="text-muted">${json[i].description}</p>
+
 <p class="mb-2 fw-bold">$${json[i].price}</p>
 <div class="d-flex">
   <i class="fas fa-circle text-info me-2 fs-5"></i>
@@ -231,19 +51,14 @@ let selValue = document.querySelector('#selection');
     container.innerHTML += template;
 })
 }
+generateColls();
 
+twoCols.addEventListener('click', () => container.className = "row row-cols-lg-2 row-cols-md-2 row-cols-sm-2");
 
-twoCols.addEventListener('click', generateTwoCols);
+threeCols.addEventListener('click', () => container.className = "row row-cols-lg-3 row-cols-md-2 row-cols-sm-2");
 
-threeCols.addEventListener('click', generateThreeCols);
+fourColumn.addEventListener('click', () => container.className = "row row-cols-lg-4 row-cols-md-2 row-cols-sm-2");
 
-fourColumn.addEventListener('click', generate);
+fullColsGenerate.addEventListener('click', generateColls);
 
-fullColsGenerate.addEventListener('click', fullColls);
-
-sortFourCols.addEventListener('click', generate);
-
-
-
-
-
+sortFourCols.addEventListener('click', () => container.className = "row row-cols-lg-4 row-cols-md-2 row-cols-sm-2");
